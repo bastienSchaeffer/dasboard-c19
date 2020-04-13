92024206af115da037d80fe549bcc172b33606e5
Bastien Schaeffer <bast.schaeffer@gmail.com>
2020-04-13 13:24:58 +0100

    feature/scraping: step1

--- src/layouts/Topbar/Topbar.tsx
+++ src/layouts/Topbar/Topbar.tsx
@@ -6,7 +6,7 @@ const Topbar = () => {
     <AppBar>
       <Toolbar>
         <Typography gutterBottom variant='h1'>
-          Dashboard Countries
+          New Dashboard Countries
         </Typography>
       </Toolbar>
     </AppBar>
