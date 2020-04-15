43f26bd5752073d3cde19e777b45e380d80b242a
Bastien Schaeffer <bast.schaeffer@gmail.com>
2020-04-15 08:14:33 +0100

    feature/scaffold: remove rechart

--- src/components/Charts/LineChart.tsx
+++ src/components/Charts/LineChart.tsx
@@ -1,14 +1,14 @@
 import React, {useState, useEffect} from 'react';
-import {
-  LineChart as LineChartRecharts,
-  Line,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-  ResponsiveContainer,
-} from 'recharts';
+// import {
+//   LineChart as LineChartRecharts,
+//   Line,
+//   XAxis,
+//   YAxis,
+//   CartesianGrid,
+//   Tooltip,
+//   Legend,
+//   ResponsiveContainer,
+// } from 'recharts';
 
 type Config = {
   key: string;
@@ -36,28 +36,47 @@ const LineChart: React.FC<LineChartProps> = ({
   }, [dataSet, daysSelected]);
 
   return (
-    <ResponsiveContainer width='100%' height={360} debounce={0}>
-      <LineChartRecharts
-        data={chartData}
-        margin={{top: 20, right: 50, left: 0, bottom: 20}}
-      >
-        <XAxis dataKey='date' />
-        <YAxis />
-        <CartesianGrid strokeDasharray='3 3' />
-        <Tooltip />
-        <Legend />
-        {config.map((item) => (
-          <Line
-            key={item.key}
-            type='monotone'
-            dataKey={item.key}
-            stroke={item.color}
-            activeDot={{r: 2}}
-            strokeWidth={4}
-          />
-        ))}
-      </LineChartRecharts>
-    </ResponsiveContainer>
+    <div>
+      {/* {config.map((item) => (
+        <div>
+          <p>{item.color}</p>
+          <p>{item.key}</p>
+        </div>
+      ))} */}
+      {/* {chartData &&
+        chartData.map((item) => (
+          <div>
+            <pre>{JSON.stringify(item, null, 2)}</pre>
+          </div>
+        ))} */}
+      <pre>
+        {JSON.stringify(config, null, 2)}
+        {JSON.stringify(chartData, null, 2)}
+      </pre>
+    </div>
+
+    // <ResponsiveContainer width='100%' height={360} debounce={0}>
+    //   <LineChartRecharts
+    //     data={chartData}
+    //     margin={{top: 20, right: 50, left: 0, bottom: 20}}
+    //   >
+    //     <XAxis dataKey='date' />
+    //     <YAxis />
+    //     <CartesianGrid strokeDasharray='3 3' />
+    //     <Tooltip />
+    //     <Legend />
+    //     {config.map((item) => (
+    //       <Line
+    //         key={item.key}
+    //         type='monotone'
+    //         dataKey={item.key}
+    //         stroke={item.color}
+    //         activeDot={{r: 2}}
+    //         strokeWidth={4}
+    //       />
+    //     ))}
+    //   </LineChartRecharts>
+    // </ResponsiveContainer>
   );
 };
 export default LineChart;
